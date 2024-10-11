07ef9723c446321110535ffe608583196e80f87a
mariam_osama <osamamariam562@gmail.com>
2024-10-11 20:02:03 +0300

    creating recipe page

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -8,9 +8,60 @@ import { IRecipe } from '../interfaces/recipes';
   styleUrl: './recipes.component.scss'
 })
 export class RecipesComponent implements OnInit {
-  recipe:IRecipe[]=[];
+  recipes:IRecipe[]=[];
+  title:string = '';
+  course:string = '';
+  cookTime:number = 0;
+  ingredients:string = '';
+  directions:string = '';
+  photoUrl:string = '';
+
   ngOnInit(): void {
     let dataService= new DataService;
-    this.recipe = dataService.getRecipe();
+    this.recipes = dataService.getRecipe();
+  }
+
+  pushRecipe(){
+    this.recipes.unshift(
+      {
+        "id": null,
+        "title": this.title,
+        "course": this.course,
+        "cuisine": null,
+        "mainIngredient": null,
+        "description": null ,
+        "source": null,
+        "url": null,
+        "urlHost": null,
+        "prepTime": null,
+        "cookTime": this.cookTime,
+        'totalTime': null,
+        'servings': null,
+        'yield': null ,
+        'ingredients': this.ingredients,
+        'directions': this.directions,
+        'rating': null,
+        'publicUrl': null,
+        'photoUrl': this.photoUrl,
+        'private': null,
+        'nutritionalScoreGeneric': null,
+        'calories':null ,
+        'fat': null ,
+        'cholesterol': null,
+        'sodium': null ,
+        'sugar': null ,
+        'carbohydrate': null,
+        'fiber': null,
+        'protein': null,
+        'cost': null,
+        'tags': null,
+      }
+    )
+    this.title = '';
+    this.course = '';
+    this.cookTime = 0;
+    this.ingredients = '';
+    this.directions = ' ';
+    this.photoUrl = ' ';
   }
 }
