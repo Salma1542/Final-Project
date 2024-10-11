5651b37bcce02baf3868cc7d78acdf3b7c0bc4d8
mariam_osama <osamamariam562@gmail.com>
2024-10-11 15:46:57 +0300

    modify appMoule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeCarsoulComponent } from './home-carsoul/home-carsoul.component';
 import { FooterComponent } from './footer/footer.component';
 import { RecipeCardsComponent } from './recipe-cards/recipe-cards.component';
-<<<<<<< HEAD
-import { RecipeCardItemComponent } from './recipe-cards/recipe-card-item/recipe-card-item.component';
-import { FormsModule } from '@angular/forms';
-=======
 import { AddPostComponent } from './add-post/add-post.component';
->>>>>>> 1e1edea2e1c34a19b2166b3e9b76575c630d2165
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
