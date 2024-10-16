395695e62472df317056c73e63b527eda3d56876
Salma1542 <123669174+Salma1542@users.noreply.github.com>
2024-10-16 15:19:16 +0300

    final-update

--- src/app/saves/saves.component.ts
+++ src/app/saves/saves.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IRecipe } from '../interfaces/recipes';
 
 @Component({
   selector: 'app-saves',
   templateUrl: './saves.component.html',
-  styleUrl: './saves.component.scss'
+  styleUrls: ['./saves.component.scss' ]  
 })
-export class SavesComponent {
+export class SavesComponent implements OnInit {
   savedRecipes: IRecipe[] = [];
 
   ngOnInit(): void {
-    const savedData = localStorage.getItem('cartRecipe');
-    if (savedData) {
-      this.savedRecipes = JSON.parse(savedData);
-    } else {
-      this.savedRecipes = []; 
+    if (typeof window !== 'undefined') {  
+      const savedData = localStorage.getItem('cartRecipe');
+      if (savedData) {
+        this.savedRecipes = JSON.parse(savedData);
+      } else {
+        this.savedRecipes = []; 
+      }
     }
   }
 }
